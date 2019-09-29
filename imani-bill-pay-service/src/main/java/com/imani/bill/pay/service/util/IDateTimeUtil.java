a6c320fb7a54e57cf7c2940f8134056a823e744b
manyce400 <fallon12>
2019-09-29 00:31:31 -0400

    Adding full working integration with Hibernate and MySQL

---
+++ imani-bill-pay-service/src/main/java/com/imani/bill/pay/service/util/IDateTimeUtil.java
@@ -0,0 +1,17 @@
+package com.imani.bill.pay.service.util;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author manyce400
+ */
+public interface IDateTimeUtil {
+
+
+    public DateTime getDateTimeAtStartOfMonth(DateTime dateTime);
+
+    public DateTime getDateTimeAtEndOfMonth(DateTime dateTime);
+
+    public Integer getDaysBetweenDates(DateTime start, DateTime end);
+
+}
---
+++ imani-bill-pay-service/src/test/java/com/imani/bill/pay/service/util/DateTimeUtilTest.java
@@ -0,0 +1,46 @@
+package com.imani.bill.pay.service.util;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author manyce400 
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DateTimeUtilTest {
+    
+    
+    @InjectMocks
+    private DateTimeUtil dateTimeUtil;
+
+    public static final DateTimeFormatter DEFAULT_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    
+    @Test
+    public void testGetDateTimeAtStartOfMonth() {
+        DateTime dateTime = dateTimeUtil.getDateTimeAtStartOfMonth(DateTime.parse("2019-09-03 09:00:00", DEFAULT_FORMATTER));
+        String dateTimeAtStartOfMonth = DEFAULT_FORMATTER.print(dateTime);
+        Assert.assertEquals("2019-09-01 00:00:00", dateTimeAtStartOfMonth);
+    }
+    
+    @Test
+    public void testGetDateTimeAtEndOfMonth() {
+        DateTime dateTime = dateTimeUtil.getDateTimeAtEndOfMonth(DateTime.parse("2019-09-03 09:00:00", DEFAULT_FORMATTER));
+        String dateTimeAtEndOfMonth = DEFAULT_FORMATTER.print(dateTime);
+        Assert.assertEquals("2019-09-30 00:00:00", dateTimeAtEndOfMonth);
+    }
+
+    @Test
+    public void testGetDaysBetweenDates() {
+        DateTime start = DateTime.parse("2019-09-03 09:00:00", DEFAULT_FORMATTER);
+        DateTime end = DateTime.parse("2019-09-05 09:00:00", DEFAULT_FORMATTER);
+        int daysBetween = dateTimeUtil.getDaysBetweenDates(start, end);
+        Assert.assertEquals(2, daysBetween);
+    }
+}
